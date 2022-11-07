5751c53620fb1f61417ebb903ce2f35b3c8734a3
NgoThanhPhong <phongngo290902@gmail.com>
2022-11-07 23:53:19 +0700

    add

---
+++ src/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <NavLink className="dropdown-item" to="/trangchu">
+            Trang chu film
+          </NavLink>
+        </li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/TrangChuMovie/TrangChu.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TrangChu() {
+  return <div className="bg-zinc-700"></div>;
+}
